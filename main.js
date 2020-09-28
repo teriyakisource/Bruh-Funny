5520179e7e4c91690a77456381593a1318c07f47
teriyakisource <vassilisnin@gmail.com>
2020-09-28 14:37:01 +0100

    more engineering

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ window.postReqAmount = 3;
 function getAll() {
     for (i = 1; i <= window.postReqAmount; i++) {
         console.log("running loop")
-        firebase.database().ref("Post/" + i + "/Url").on('value', function (snapshot) {
-            console.log(snapshot.val())
-            document.getElementById("stream").innerHTML += 
-            "<br><img src = '" + snapshot.val() + "' width = '70%'/><br>" 
+        var returnval = []
+        firebase.database().ref("Post/" + i).on('value', function (snapshot) {
+            returnval += snapshot.val
+
+            
         })
+
+        console.log("Returnval: ", returnval)
     }
 }
 
