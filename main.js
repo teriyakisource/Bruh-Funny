6448c80b342caf8c5674f267b400b7681206a622
teriyakisource <vassilisnin@gmail.com>
2020-09-28 11:18:23 +0100

    added names to images

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ window.postReqAmount = 3;
 function getAll() {
     for (i = 1; i <= window.postReqAmount; i++) {
         console.log("running loop")
-        firebase.database().ref("Post/" + i + "/Url").on('value', function (snapshot) {
+        var content = []
+        firebase.database().ref("Post/" + i).on('value', function (snapshot) {
+            content += snapshot.val();
             console.log(snapshot.val())
-            document.getElementById("stream").innerHTML += 
-            "<br><img src = '" + snapshot.val() + "' width = '200'/>" 
+            
         })
+        document.getElementById("stream").innerHTML += 
+            "<br><br><p>" + content[0] + "<br><img src = '" + content[1] + "' width = '200'/>" 
     }
 }
 
