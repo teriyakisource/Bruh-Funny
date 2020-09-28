689065a569334e6d11e69d09e74ea1847579583f
teriyakisource <vassilisnin@gmail.com>
2020-09-28 14:55:19 +0100

    size

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function getAll() {
             snapshot.val()["Name"]
 
             document.getElementById("stream").innerHTML += 
-            "<br><br><div class = 'post'><h3>" + snapshot.val()["Name"] + "</h3></div><br><img src = '" + snapshot.val()["Url"] + "' width = 60%/><br>"
+            "<br><br><div class = 'post'><h3>" + snapshot.val()["Name"] + "</h3></div><br><img src = '" + snapshot.val()["Url"] + "' width = 30%/><br>"
             
         })
 
