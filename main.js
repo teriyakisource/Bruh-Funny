79e01ba4ab1e316a9cbe0fd5aa57722eba11b0f3
teriyakisource <vassilisnin@gmail.com>
2020-09-28 10:59:27 +0100

    debugging notes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.postReqAmount = 1;
 function getAll() {
     for (i = 1; i <= window.postReqAmount; i++) {
         firebase.database().ref("Post/" + window.postReqAmount + "/Url").on('value', function (snapshot) {
+            console.log(snapshot.val())
             document.getElementById("stream").innerHTML += 
             "<br><img src = '" + snapshot.val() + "'/>" 
         })
