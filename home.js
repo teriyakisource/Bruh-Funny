d5da03524f36cfead6f8b2dba9a3ebd64d84538c
teriyakisource <vassilisnin@gmail.com>
2020-09-28 10:56:50 +0100

    fixed some theoretical bugs in grabing posts

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
-window.postReqAmount = 30;
+window.postReqAmount = 1;
 
 function getAll() {
-    for (i = 1; i < window.postReqAmount; i++) {
-        firebase.database().ref("Post/" + window.postReqAmount + "/imgurl").on('value', function (snapshot) {
+    for (i = 1; i <= window.postReqAmount; i++) {
+        firebase.database().ref("Post/" + window.postReqAmount + "/Url").on('value', function (snapshot) {
             document.getElementById("stream").innerHTML += 
             "<br><img src = '" + snapshot.val() + "'/>" 
         })
