567504509d8f5b80e53f2b3ccf1914a3649834b5
teriyakisource <vassilisnin@gmail.com>
2020-09-28 14:54:38 +0100

    size

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function getAll() {
         firebase.database().ref("Post/" + i).on('value', function (snapshot) {
             snapshot.val()["Name"]
 
-            document.getElementById("stream").innerHTML = 
-            "<br><br><div class = 'post'><h3>" + snapshot.val()["Name"] + "</h3></div><br><img src = '" + snapshot.val()["Url"] + "'/><br>"
+            document.getElementById("stream").innerHTML += 
+            "<br><br><div class = 'post'><h3>" + snapshot.val()["Name"] + "</h3></div><br><img src = '" + snapshot.val()["Url"] + "' width = 60%/><br>"
             
         })
 
