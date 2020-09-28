7ee5a51fdc0146ff3a60d80d20a8fe8538272299
teriyakisource <vassilisnin@gmail.com>
2020-09-28 10:23:57 +0100

    partially broken but made a lot of files.

---
+++ src/firebaseconfig.js
@@ -0,0 +1,12 @@
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD4Dc31Ls8L8RHeZoMNM8rd883bqWS8_xE",
+    authDomain: "bruhfunny-4aec4.firebaseapp.com",
+    databaseURL: "https://bruhfunny-4aec4.firebaseio.com",
+    projectId: "bruhfunny-4aec4",
+    storageBucket: "bruhfunny-4aec4.appspot.com",
+    messagingSenderId: "761653717859",
+    appId: "1:761653717859:web:ce472800e9624432f34156"
+};
+firebase.initializeApp(firebaseConfig);
