1d44df1d69a613821e0e3c77dd03db51df66957a
teriyakisource <vassilisnin@gmail.com>
2020-09-28 14:51:47 +0100

    new style of post stream

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ window.postReqAmount = 3;
 function getAll() {
     for (i = 1; i <= window.postReqAmount; i++) {
         console.log("running loop")
-        var returnval = []
+        
         firebase.database().ref("Post/" + i).on('value', function (snapshot) {
-            returnval += snapshot.val() 
-            console.log(snapshot.val()["Name"])
+            snapshot.val()["Name"]
 
+            document.getElementById("stream").innerHTML = 
+            "<br><br><div class = 'post'><h3>" + snapshpt.val()["Name"] + "</h3></div><br><img src = " + snapshot.val()["Url"] + "<br>"
             
         })
 
