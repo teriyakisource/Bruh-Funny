f2a7d12e8b31abf85fbdf5c3096fae20eabcb3f5
teriyakisource <vassilisnin@gmail.com>
2020-09-28 11:05:21 +0100

    debugging

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.postReqAmount = 2;
 function getAll() {
     for (i = 0; i < window.postReqAmount; i++) {
         console.log("running loop")
-        firebase.database().ref("Post/" + window.postReqAmount + "/Url").on('value', function (snapshot) {
+        firebase.database().ref("Post/" + i + "/Url").on('value', function (snapshot) {
             console.log(snapshot.val())
             document.getElementById("stream").innerHTML += 
             "<br><img src = '" + snapshot.val() + "'/>" 
